620874925e0769b9d8b8b2122a20957e6cae0a89
Veronica Rudiuk <veronicarudiuk@gmail.com>
2022-10-13 19:50:24 +0400

    Initial Commit

---
+++ Angela Project 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Angela Project 1
+//
+//  Created by Veronica Rudiuk on 13.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
